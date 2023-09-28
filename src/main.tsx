20c3f33ab53bc8b5c0d1453d639616ff8f10a0ba
Ryan Hummel <112951711+ryanrasal@users.noreply.github.com>
2023-09-28 12:10:53 +0200

    finish home

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Routes from "./routes/index";
+import "./App.css";
 
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  const router = createBrowserRouter(Routes);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        bgHome: "url('./src/assets/bgHome.jpg')",
+        cardHome: "url('./assets/fondbois.png')",
+      },
+    },
   },
   plugins: [],
 };
